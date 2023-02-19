0d8736bacae766a100fe1c2ee8edf1cd3bd1e3cd
Max Harold <maximilian.harold@gmail.com>
2023-02-19 15:59:55 -0600

    potential working tilt calculations?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.data.CSVManager;
 import frc.robot.math.*;
 
@@ -30,6 +31,8 @@ import static frc.robot.RobotContainer.m_chassis;
  * project.
  */
 public class Robot extends TimedRobot {
+
+  
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
--- src/main/java/frc/robot/math/Tilt.java
+++ src/main/java/frc/robot/math/Tilt.java
@@ -3,7 +3,10 @@ package frc.robot.math;
 import static java.lang.Math.*;
 
 public class Tilt {
-    public static double calculate(double pitch, double roll) {
-        return toDegrees(acos(cos(toRadians(pitch)) * cos(toRadians(roll))));
+    public static double calculate(double yaw, double pitch, double roll) {
+        double sign = 1;
+        if (sin(toRadians(yaw)) * roll < 1 || cos(toRadians(pitch)) * pitch < 1)
+            sign = -1;
+        return sign * toDegrees(acos(cos(toRadians(pitch)) * cos(toRadians(roll))));
     }
 }
