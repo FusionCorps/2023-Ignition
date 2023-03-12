685aca8c1b9d236c3068a9ebfa2da1d29a7260d2
NPerumandla <24perumandlan@cistercian.org>
2023-03-11 19:07:14 -0600

    Wire Side Auton

--- src/main/java/frc/robot/commands/autos/TwoPieceHighWire.java
+++ src/main/java/frc/robot/commands/autos/TwoPieceHighWire.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.ArmToPosition;
 import frc.robot.commands.arm.TwoPartHigh;
@@ -47,11 +48,17 @@ public class TwoPieceHighWire extends SequentialCommandGroup {
                 new TwoPartHighAuto(m_arm),
                 new ArmToPosition(m_arm,HIGH_BASE_POS_ALT_AUTO,HIGH_WRIST_POS_ALT_AUTO),
                 new RunVoltsTime(m_intake,OUTTAKE_VOLTS,0.5),
-                new ArmToPosition(m_arm,BASE_START_POS,WRIST_START_POS),
-                m_chassis.followTrajectoryCommand(twoPieceWireSideA,true),
-                m_chassis.followTrajectoryCommand(twoPieceWireSideB,false),
+                new ParallelCommandGroup(
+                        new ArmToPosition(m_arm,INTAKE_BASE_POS_CUBE,INTAKE_WRIST_POS_CUBE),
+                        m_chassis.followTrajectoryCommand(twoPieceWireSideA,true),
+                        new RunVoltsTime(m_intake,-6,twoPieceWireSideA.getTotalTimeSeconds())
+                ),
+                new ParallelCommandGroup(
+                        new ArmToPosition(m_arm,BASE_START_POS,WRIST_START_POS),
+                        m_chassis.followTrajectoryCommand(twoPieceWireSideB,false)
+                ),
                 new ArmToPosition(m_arm,HIGH_BASE_POS,HIGH_WRIST_POS),
-                new ChassisDriveAuton(m_chassis,0.2,0,0,0.1),
+                new ChassisDriveAuton(m_chassis,0.2,0,0,0.2),
                 new RunVoltsTime(m_intake,OUTTAKE_VOLTS,0.2),
                 new ChassisDriveAuton(m_chassis,-0.2,0,0,0.1),
                 new ArmToPosition(m_arm,BASE_START_POS,WRIST_START_POS)
